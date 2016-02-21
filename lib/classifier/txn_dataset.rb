6f0939521df954dbf67f48ea4959f1216e2e19c7
Daniel Fone <daniel@fone.net.nz>
2016-02-22 01:50:26 +1300

    75% success rate

--- lib/classifier/txn_dataset.rb
+++ lib/classifier/txn_dataset.rb
@@ -9,47 +9,70 @@ module Budgetary::Classifier
     # - day of month?
     # - day of week?
 
+    AMOUNT_BIN_SIZE = 50
+
     def initialize
-      @category_counts = Hash.new 1
-      @amount_counts = Hash.new 1 # experiment with bin size
-      @amount_features = Hash.new Hash.new 1
-      @memo_count = Hash.new Hash.new 1
-      @memo_features = Hash.new Hash.new 1
-      @total_count = 1
+      @category_counts = Hash.new 0
+      @category_priors = Hash.new 0
+      @amount_features = Hash.new { |h,k| h[k] = Hash.new 0 }
+      @memo_features = Hash.new { |h,k| h[k] = Hash.new 0 }
+      @total_count = 0
     end
 
     def train(category, txn)
-      @category_counts[category] += 1
       @total_count += 1
+
+      @category_counts[category] += 1
+
+      amount = amount_bin txn['amount']
+      @amount_features[category][amount] += 1
+
+      memo_bins(txn['memo']).each do |m|
+        @memo_features[category][m] += 1
+      end
     end
 
     def classify(txn)
-      probabilities = categories.each_with_object({}) { |c,h| h[c] = score txn, c }
-      category, count = probabilities.sort_by(&:last).last # sort by probability, get the highest (last)
+      category, count = category_distribution(txn).last # sort by probability, get the highest (last)
       # need to check distribution, don't pick a category if it's uncertain
       category
+    end
+
+    def category_distribution(txn)
+      probabilities = categories.each_with_object({}) { |c,h| h[c] = score txn, c }
+      probabilities.sort_by(&:last) # sort by probability
     end
 
     def categories
       @categories ||= @category_counts.keys
     end
 
-  private
+#  private
 
     # P(txn|category)
     def score(txn, category)
-      category_prior = @category_counts[category] / @total_count
-    #  observation.reduce(1) { |p,f,v| p * probability(f, v, category) } * category_probability
+      category_prior = @category_counts[category] / @total_count.to_f
+      memo_likelihood(txn, category) * amount_likelihood(txn, category) * category_prior
     end
 
-    ## P(feature|category)
-    #def probability(feature, value, category)
-    #  tokens = value.split(/\s/)
-    #  tokens.reduce(1).each do |p,token|
-    #    # P(memo contains "FISH"|category="Holiday")
-    #    p * (features[category][token] / features[category])
-    #  end
-#
-    #end
+    def amount_likelihood(txn, category)
+      amount = amount_bin txn['amount']
+      @amount_features[category][amount] / @category_counts[category].to_f
+    end
+
+    def memo_likelihood(txn, category)
+      memo_bins(txn['memo']).reduce(1) do |p,m|
+        p * (@memo_features[category][m] / @category_counts[category].to_f)
+      end
+    end
+
+    def amount_bin(amount)
+      amount.to_i / AMOUNT_BIN_SIZE
+    end
+
+    def memo_bins(memo)
+      memo.downcase.split
+    end
+
   end
 end
